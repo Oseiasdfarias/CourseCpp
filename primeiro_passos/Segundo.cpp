d6fae8cecd8af5f92af4dd09fffe97d13fdc69ef
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-06-05 01:41:04 -0300

    config pastas

---
+++ primeiro_passos/Segundo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Oséias Farias
+
+/* Imprimindo no terminal
+ * a frase VOU APRENDER C++ */
+
+int main( int argc, char **argv ){
+
+	std :: cout << "VOU \nAPRENDER\nC++" << std :: endl;
+
+	return 0;
+}
