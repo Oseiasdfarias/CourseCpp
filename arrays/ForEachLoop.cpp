6a1701bd908228b824066be6175eb4a2e36c4ff0
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-06-21 16:33:28 -0300

    atualização exericios de POO

--- arrays/ForEachLoop.cpp
+++ arrays/ForEachLoop.cpp
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
 	int Anos[] {1993, 2000, 2012, 2021, 2022};
 	for (auto Ano: Anos){
-		std::cout << Ano << "\n";
+		std::cout << "Ano de " << Ano << "\n";
 	}
 	return 0;
 }
